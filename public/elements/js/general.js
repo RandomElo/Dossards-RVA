b5338bdf5d25784e7247c821a67dbf351f20926c
Eloi <eloi.random@gmail.com>
2024-03-28 20:18:23 +0100

    Mise à jour responsive #6 - Changement de navbar

--- public/elements/js/general.js
+++ public/elements/js/general.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const utilisationDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
     const favicon = document.querySelector('link[rel="icon"]');
+    const iconeHamburger = document.querySelector('#imageHamburger')
 
     if (utilisationDark) {
         favicon.href = "/public/img/favicon-light.ico";
@@ -8,3 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
         favicon.href = "/public/img/favicon-dark.ico";
     }
 });
+
+//Gestion de la navbar 
+//Récupération du menu hamburger
+const menuHamburger = document.querySelector(".menuHamburger")
+//Récupération du navlink
+const navLinks = document.querySelector(".navLinks")
+//Ajout de l'événment click sur l'image du menu hamburger
+menuHamburger.addEventListener('click',()=>{
+	navLinks.classList.toggle('mobile-menu')
+}) 
